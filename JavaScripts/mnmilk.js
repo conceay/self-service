7e49debb384efad21c6df208df5f9920696cf221
gentleMoon <76985627+conceay@users.noreply.github.com>
2024-02-21 22:42:37 +0800

    Create mnmilk.js

---
+++ JavaScripts/mnmilk.js
@@ -0,0 +1,21 @@
+/* 
+蒙牛兑奶
+*******************************
+[rewrite_local]
+# > 修改返回体，使按钮变亮
+^http[s]?:\/\/mp-isv\.youzanyun\.com\/point\/item\/getItemDetail url script-response-body mnmilk.js
+
+*******************************
+[mitm]
+hostname = mp-isv.youzanyun.com
+*/
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.data.isExchange = True;
+
+mn = JSON.stringify(obj);
+
+$done({
+    mn
+});
