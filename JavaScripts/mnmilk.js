205322adefa03399c7244916736deb785a3b08c2
gentleMoon <76985627+conceay@users.noreply.github.com>
2024-02-21 22:53:57 +0800

    Update mnmilk.js

--- JavaScripts/mnmilk.js
+++ JavaScripts/mnmilk.js
@@ -3,9 +3,7 @@
 *******************************
 [rewrite_local]
 # > 修改返回体，使按钮变亮
-^http[s]?:\/\/mp-isv\.youzanyun\.com\/point\/item\/getItemDetail url script-response-body https://raw.githubusercontent.com/conceay/self-service/main/JavaScripts/mnmilk.js
-
-*******************************
+^http[s]?:\/\/mp-isv\.youzanyun\.com\/point\/item\/getItemDetail url script-response-body https://raw.githubusercontent.com/conceay/self-service/main/JavaScripts/mnmilk.js 
 [mitm]
 hostname = mp-isv.youzanyun.com
 */
